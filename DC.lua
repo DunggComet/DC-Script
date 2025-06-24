8a5283af901846c321ec21a084560539b2625dda
DunggKR <Dunggkr@gmail.com>
2025-06-24 14:28:24 +0700

    Update DC.lua

--- DC.lua
+++ DC.lua
@@ -1,67 +1,68 @@
--- Dragon City Mod Menu Script
--- Made by Dungg
+-- Combined Dragon City Ultimate Mod with Test Skill Integration &amp; Revert Paste Feature
 gg.setVisible(false)
 
--- Update Checker
-local pth, opth = "/storage/emulated/0/Download/1.8.0.txt", "/storage/emulated/0/Download/1.7.0.txt"
-local fl = io.open(pth, "r")
-if not fl then
-  gg.alert("A new update V1.8.0 is available! Please update now!\nNew: 🔵Expand🔵\nCách sử dụng: bật chức năng 🔵Expand all Unlocked🔵 sau đó vào Ancient Portal rồi về để hiệu lực\nRemoved 📙Goals📙 🔵Server Hack🔵", "Update Now")
-  gg.toast("Updating...")
-  gg.searchNumber(":updateversion", gg.TYPE_AUTO)
-  gg.sleep(2000)
-  os.remove(opth)
-  local nF = io.open(pth, "w")
-  if nF then
-    nF:write("Dungg V1.8.0")
-    nF:close()
-    gg.alert("Update successful!")
-  else
-    gg.alert("Error during update!")
-  end
+local path = "/storage/emulated/0/Download/1.8.0.txt"
+local oldPath = "/storage/emulated/0/Download/1.7.0.txt"
+local file = io.open(path, "r")
+
+if file then
+    file:close()
 else
-  fl:close()
+    gg.alert("A new update V1.8.0 is available! Please update now!\nNew: 🔵Expand🔵\nCách sử dụng: bật chức năng 🔵Expand all Unlocked🔵 sau đó vào Ancient Portal rồi về để hiệu lực\nRemoved 📙Goals📙 🔵Server Hack🔵", "Update Now")
+    
+    gg.toast("Updating...")
+    gg.searchNumber(":updateversion", gg.TYPE_AUTO)
+    gg.sleep(2000)
+    
+    -- Delete old version file
+    os.remove(oldPath)
+    
+    local newFile = io.open(path, "w")
+    if newFile then
+        newFile:write("Dungg V1.8.0")
+        newFile:close()
+        gg.alert("Update successful!")
+    else
+        gg.alert("Error during update!")
+    end
 end
 
 gg.setVisible(false)
-
--- Main menu handler
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
 local MD5 = gg.makeRequest('').content
 function Main()
-  local opt = gg.choice({
-    '⚔Battle Arena Mod⚔',
-    '🏟Unmod Battle Arena🏟',
-    '🏟Quest Mod🏟',
-    '🏟Recall Dragon🏟',
-    '⚔Rescue⚔',
-    '🕹Test/Train Skill Mod🕹',
-    '🔵Event Battle Speed🔵',
-    '🔵Exit🔵'
-  }, nil, 'Dragon City Tools\ncoded by Dungg DC')
-
-  if opt == 1 then DragonCityHack1()
-  elseif opt == 2 then DragonCityHack2()
-  elseif opt == 3 then DragonCityHack3()
-  elseif opt == 4 then DragonCityHack4()
-  elseif opt == 5 then DragonCityHack5()
-  elseif opt == 6 then DragonCityHack6()
-  elseif opt == 7 then DragonCityHack7()
-  elseif opt == 8 then DragonCityHack8()
-  elseif opt == nil then NoSelect()
-  end
+menu = gg.choice({
+'⚔Battle Arena Mod⚔',
+'🏟Unmod Battle Arena🏟',
+'🏟Quest Mod🏟',
+'🏟Recall Dragon🏟',
+'⚔Rescue⚔',
+'🕹Test/Train Skill Mod🕹',
+'🏃‍♂️Event Battle Speed🏃‍♂️',
+'Exit'
+},nil, 'Dragon City Tools\nScript made by Dungg')
+if menu == 1 then DragonCityHack1() end
+if menu == 2 then DragonCityHack2() end
+if menu == 3 then DragonCityHack3() end
+if menu == 4 then DragonCityHack4() end
+if menu == 5 then DragonCityHack5() end
+if menu == 6 then DragonCityHack6() end
+if menu == 7 then DragonCityHack7() end
+if menu == 8 then DragonCityHack8() end
+if menu ==nil then NoSelect() end
 end
-
--- Feature handlers
 function DragonCityHack1()
-  local L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Arena').content
-  if not L then gg.alert('SERVER: Allow Internet Connection...') else pcall(load(L)) end
+L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Arena').content
+if not L then gg.alert('SERVER: Allow Internet Connection...') else
+pcall(load(L)) end
 end
 
 function DragonCityHack2()
   gg.setVisible(false)
-  local frz = gg.getListItems()
-  if #frz > 0 then
-    gg.removeListItems(frz)
+  local frozenItems = gg.getListItems()
+  if #frozenItems &gt; 0 then
+    gg.removeListItems(frozenItems)
     gg.toast('All values have been unfrozen!')
   else
     gg.toast('No frozen values found!')
@@ -69,44 +70,48 @@ function DragonCityHack2()
 end
 
 function DragonCityHack3()
-  local L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Quest').content
-  if not L then gg.alert('SERVER: Allow Internet Connection...') else pcall(load(L)) end
+L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Quest').content
+if not L then gg.alert('SERVER: Allow Internet Connection...') else
+pcall(load(L)) end
 end
 
 function DragonCityHack4()
-  local L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Recall').content
-  if not L then gg.alert('SERVER: Allow Internet Connection...') else pcall(load(L)) end
+L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Recall').content
+if not L then gg.alert('SERVER: Allow Internet Connection...') else
+pcall(load(L)) end
 end
 
 function DragonCityHack5()
-  local L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Rescue').content
-  if not L then gg.alert('SERVER: Allow Internet Connection...') else pcall(load(L)) end
+L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Rescue').content
+if not L then gg.alert('SERVER: Allow Internet Connection...') else
+pcall(load(L)) end
 end
 
 function DragonCityHack6()
-  local L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Train%20Skill').content
-  if not L then gg.alert('SERVER: Allow Internet Connection...') else pcall(load(L)) end
+L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Train%20Skill').content
+if not L then gg.alert('SERVER: Allow Internet Connection...') else
+pcall(load(L)) end
 end
 
 function DragonCityHack7()
-  local L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Event%20Battle%20Skip').content
-  if not L then gg.alert('SERVER: Allow Internet Connection...') else pcall(load(L)) end
+L = gg.makeRequest('https://raw.githubusercontent.com/DunggKR/DC-Script/main/Folder/Event%20Battle%20Skip').content
+if not L then gg.alert('SERVER: Allow Internet Connection...') else
+pcall(load(L)) end
 end
 
 function DragonCityHack8()
-  os.exit()
+os.exit()
 end
 
 function NoSelect()
-  gg.sleep(100)
-  gg.toast('coded by Dungg DC')
+gg.sleep(100)
+gg.toast('Script made by Dungg')
 end
 
--- Loop main
+-------------------------------------------------------------------------
 gg.setVisible(true)
 while true do
-  if gg.isVisible() then
-    gg.setVisible(false)
-    Main()
-  end
-end
+if gg.isVisible() then
+gg.setVisible(false)
+Main() end end
+-------------------------------------------------------------------------
