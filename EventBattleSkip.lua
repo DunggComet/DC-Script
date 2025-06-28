a9f432dce17eaf4caa9ecd230574720ca1b17e8b
DunggKR <Dunggkr@gmail.com>
2025-06-29 03:03:14 +0700

    Update EventBattleSkip.lua

--- EventBattleSkip.lua
+++ EventBattleSkip.lua
@@ -228,7 +228,7 @@ end
 	  gg.sleep(20000)
     end
     if input[10] then
-		L = gg.makeRequest('https://dunggkr.github.io/DC-Script/DC.lua',nil,'https://dragoncitytips.com/scripts').content
+		L = gg.makeRequest('https://dunggkr.github.io/DC-Script/DC.lua').content
 		if not L then gg.alert('SERVER: Allow Internet Connection...') else
 		pcall(load(L)) end
     end
