27aca94a45cf4099715b9f4f2fd8949b2a288ac2
DunggKR <Dunggkr@gmail.com>
2025-06-29 03:34:30 +0700

    Update Help.lua

--- Help.lua
+++ Help.lua
@@ -1,1 +1,14 @@
-gg.alert("There's no help, beach )
+-- Game Guardian Script: Discord Help Copier
+gg.alert("Starting...")
+
+-- Set your Discord username here:
+local discordUsername = "dunggkr"
+
+-- Copy to clipboard
+gg.copyText(discordUsername)
+
+-- Alert copied
+gg.alert("Copied Discord's username for help!")
+
+-- Final message
+gg.alert("Message Dunggkr ( Comet ) in discord to get help with the script.")
