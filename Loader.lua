bb23ee9f037f43fc6dca242f61841540335e5d1e
DunggVN <Dunggkr@gmail.com>
2025-07-17 17:31:34 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 -- 🛡️ Passwords with expiry datetime (per user)
 local Passwords = {
   khoa = {
-    message = "✅ Hé lô, test script vui vẻ nhé anh zai",
+    message = "✅ Thôi thoát script đi, chơi bẩn làm gì. Tắt script chơi công bằng đi=))",
     url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua",
-    expiry = { year = 2025, month = 7, day = 1, hour = 0, min = 0, sec = 0 }
+    expiry = { year = 2025, month = 8, day = 1, hour = 0, min = 0, sec = 0 }
   },
   NIKA = {
     message = "✅ Hello NIKA!",
