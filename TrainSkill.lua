a01e727d9bef640114d1071423cb3a9e1bbfe452
DunggVN <Dunggkr@gmail.com>
2025-07-21 03:37:33 +0700

    Update TrainSkill.lua

--- TrainSkill.lua
+++ TrainSkill.lua
@@ -48,7 +48,7 @@ local function safePromptLoop(prompts, defaults, types)
 end
 
 local function fetchDragonData()
-  local response = gg.makeRequest("https://dunggcomet.github.io/DC-Script/Website/Dragon.html")
+  local response = gg.makeRequest("https://dunggcomet.github.io/DC-Script/Website/Dragon")
   if not response or not response.content then
     gg.alert("Failed to download dragon data!")
     return nil
