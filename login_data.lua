31da73ba57019acb45eb82fed94812c86b9f9930
DunggVN <Dunggkr@gmail.com>
2025-07-23 00:59:24 +0700

    Update login_data.lua

--- login_data.lua
+++ login_data.lua
@@ -4,19 +4,16 @@ return function()
     khoa = {
       expiry = "01082025",
       message = "✅ Tắt script đi!!! \nChơi công bằng mới vuii ( xạo đó ).",
-      expired_message = "❌ Hết hạn rồi cha ơi, làm ơn gia hạn bằng cách cho orb để tiếp tục sài script :>.",
       url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
     },
-    NIKA = {
-      expiry = "20072025",
-      message = "✅ Hello NIKA!",
-      expired_message = "❌ Xin lỗi NIKA, script của bạn đã hết hạn rồi!",
+    vodat = {
+      expiry = "24072025",
+      message = "✅ Hello=))\n🔑 Key: vodat",
       url = "https://raw.githubusercontent.com/DunggKR/DC-Script/main/nika.lua"
     },
-    742391 = {
+    dungg = {
       expiry = "31122099",
       message = "✅ Chào Chủ Nhân!\nMong dùng tốt ạ!",
-      expired_message = "❌ wtf? hết hạn được luôn à?",
       url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
     }
   }
