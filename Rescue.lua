7182bbaea217d0b7485d8ecc845891237b5c1d89
DunggVN <Dunggkr@gmail.com>
2025-07-23 23:20:27 +0700

    Update Rescue.lua

--- Rescue.lua
+++ Rescue.lua
@@ -1,46 +1,47 @@
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+  gg.setVisible(true)
+  local menu = gg.choice(
+    {"🐉 Change Dragon to Terra", "📊 Change Level to 1", "✖️ Close"},
+    nil,
+    "Select an option:"
+  )
 
--------------------------------------------------------------------------
--------------------------------------------------------------------------
-function inputs()
-local input = {}
-local configFile = gg.EXT_CACHE_DIR .. '/' .. 
-gg.getFile():match('') .. 'Rescue.txt'
-local data = loadfile(configFile)
-if data ~= nil then
-data = data()
-end
-input = gg.prompt({'⬇️Change Dragon⬇️','Change Dragon:','⬇️Level⬇️','Level: [1;100]','●Close●'},data,{'checkbox','number','checkbox','number','checkbox','checkbox'})
-if input ==nil then
-else
-gg.saveVariable(input, configFile)
-if input[1] then
-input[2] = input[2] 
-gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD , false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber('1000~3300', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.getResults(3000)
-gg.editAll(input[2], gg.TYPE_DWORD)
-gg.clearResults()
-end
-if input[3] then
-input[4] = input[4] 
-gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD , false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber('1~70', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.getResults(3000)
-gg.editAll(input[4], gg.TYPE_DWORD)
-gg.clearResults()
-end
-if input[5] then
-L = gg.makeRequest('https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua').content
-if not L then gg.alert('SERVER: Allow Internet Connection...') else
-pcall(load(L)) end
+  if menu == nil then
+    gg.setVisible(false)
+    return
   end
+
+  gg.saveVariable({[1]=menu==1, [2]=menu==2, [3]=menu==3}, configFile)
+
+  if menu == 1 then
+    gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.refineNumber('1000~3300', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(3000)
+    gg.editAll('1011', gg.TYPE_DWORD)
+    gg.clearResults()
+  elseif menu == 2 then
+    gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.refineNumber('1~70', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(3000)
+    gg.editAll('1', gg.TYPE_DWORD)
+    gg.clearResults()
+  elseif menu == 3 then
+    L = gg.makeRequest('https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua').content
+    if not L then
+      gg.alert('SERVER: Allow Internet Connection...')
+    else
+      pcall(load(L))
+    end
   end
-  end
+end
 
 -------------------------------------------------------------------------
 gg.setVisible(true)
 while true do
-if gg.isVisible() then
-gg.setVisible(false)
-inputs() end end
+  if gg.isVisible() then
+    gg.setVisible(false)
+    inputs()
+  end
+end
 -------------------------------------------------------------------------
