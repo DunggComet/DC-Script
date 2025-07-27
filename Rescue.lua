c2275ac65fcaa29c79b69dc0bd1036eb428ec171
DunggVN <Dunggkr@gmail.com>
2025-07-27 21:07:38 +0700

    Update Rescue.lua

--- Rescue.lua
+++ Rescue.lua
@@ -1,46 +1,15 @@
-gg.setVisible(false)
--------------------------------------------------------------------------
--------------------------------------------------------------------------
-function inputs()
-  local menu = gg.choice(
-    {"🐉 Change All Dragons to Terra", "📊 Change Level to 1", "✖️ Close Rescue Script"},
-    nil,
-    "Rescue Script Made By Comet 💫💗"
-  )
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('1000~3300', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3000)
+gg.editAll('1011', gg.TYPE_DWORD)
+gg.clearResults()
 
-  if menu == nil then
-    gg.setVisible(false)
-    return
-  end
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('1~70', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3000)
+gg.editAll('1', gg.TYPE_DWORD)
+gg.clearResults()
 
-  if menu == 1 then
-    gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    gg.refineNumber('1000~3300', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    gg.getResults(3000)
-    gg.editAll('1011', gg.TYPE_DWORD)
-    gg.clearResults()
-  elseif menu == 2 then
-    gg.searchNumber('1000~3300;-1;1~100::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    gg.refineNumber('1~70', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    gg.getResults(3000)
-    gg.editAll('1', gg.TYPE_DWORD)
-    gg.clearResults()
-  elseif menu == 3 then
-    L = gg.makeRequest('https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua').content
-    if not L then
-      gg.alert('SERVER: Allow Internet Connection...')
-    else
-      pcall(load(L))
-    end
-  end
-end
-
--------------------------------------------------------------------------
-gg.setVisible(true)
-while true do
-  if gg.isVisible() then
-    gg.setVisible(false)
-    inputs()
-  end
-end
--------------------------------------------------------------------------
+gg.alert("✅ Dragon Rescue Modification Completed!")
