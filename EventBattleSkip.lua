ecf7a477c496d014d0a140afcc8ba5b48ef7552c
DunggVN <Dunggkr@gmail.com>
2025-07-27 18:56:08 +0700

    Update EventBattleSkip.lua

--- EventBattleSkip.lua
+++ EventBattleSkip.lua
@@ -245,7 +245,7 @@ end
       gg.setVisible(true)
     end
     if input[10] then
-		L = gg.makeRequest('raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua',nil,'https://dunggcomet.github.io/DC-Script/').content
+		L = gg.makeRequest('raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua').content
 		if not L then gg.alert('SERVER: Allow Internet Connection...') else
 		pcall(load(L)) end
     end
