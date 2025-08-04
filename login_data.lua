13e13fafd75d89dc8eb4a8bd4a08153de0aeaf68
DunggVN <Dunggkr@gmail.com>
2025-08-04 16:12:36 +0700

    Update login_data.lua

--- login_data.lua
+++ login_data.lua
@@ -1,54 +1,54 @@
 -- 🧾 Return login table
 return function()
   return {
-    khoa = {
+    ["3595670854993946947"] = { -- Owner
       expiry = "01082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
     
     NIKA = {
-      expiry = "20072025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      expiry = "20092025",
+      version = "v2"
     },
     
     dungg = { -- Owner
       expiry = "31122099",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
     
     dat = {
       expiry = "01082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
     
     hamzah = { -- Hamzah
       expiry = "14082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
     
     ["3677669675036869128"] = { -- Yamuri
       expiry = "05082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
     
     ["100000787762171"] = { -- Vxdus
       expiry = "31082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
     
     ["3351274690595497851"] = { -- Khánh
       expiry = "11082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
 
     ["3428492164128316221"] = { -- Marcos
       expiry = "25082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
 
     ["100000570093971"] = { -- Gwayne
       expiry = "11082025",
-      url = "https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua"
+      version = "v1"
     },
   }
 end
