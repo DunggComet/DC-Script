1b4e7bbea2b0795823ce615a8fe5dba242a74feb
DunggVN <Dunggkr@gmail.com>
2025-08-07 17:12:07 +0700

    Update EventBattleSkip.lua

--- HeroicRace/EventBattleSkip.lua
+++ HeroicRace/EventBattleSkip.lua
@@ -247,7 +247,7 @@ end
     end
     if input[10] then
       gg.toast("Returning To Main Menu...")
-		L = gg.makeRequest('https://raw.githubusercontent.com/DunggComet/DC-Script/main/DC.lua').content
+		L = gg.makeRequest('https://raw.githubusercontent.com/DunggComet/DC-Script/main/HeroicRace/HeroicChoose.lua').content
 		if not L then gg.alert('SERVER: Allow Internet Connection...') else
 		pcall(load(L)) end
     end
